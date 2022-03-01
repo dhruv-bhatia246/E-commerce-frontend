b771290240bc23b1c63225faf16508744cfa8c4f
Dhruv <dhruv.bhatia246@gmail.com>
2022-03-02 03:05:24 +0530

    Card tilt added

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import Tilt from 'react-tilt';
 import './style.css';
 
 /**
@@ -11,7 +12,8 @@ const HomePage = (props) => {
   return (
     <>
       <Layout />
-      <div className="main-div" >
+      <Tilt className="Tilt shadow-2 br2" options={{ max: 10 }} style={{ width: "80%", padding: "10px", margin: " 50px auto", background: "linear-gradient(180deg, rgba(251,250,181,1) 0%, rgba(175,244,213,1) 50%, rgba(255,149,160,1) 100%)", borderRadius: "10px" }} >
+        {/* <div className="main-div" > */}
         <p className="p1">This web application implements the basic features of Flipkart web app and in not a complete clone of it. Hence, to test the functionlity of this, you can try the following steps:</p>
         <ol>
           <li>
@@ -44,7 +46,7 @@ const HomePage = (props) => {
             Select the address you want to deliver the product add or add new if no address is saved. You can also edit the previously saved address by selecting it and then clicking on edit button on the right side.
           </li>
           <li>
-            Now, check the order summary and review the order. Click on continue to move to the final step i.e. payment. 
+            Now, check the order summary and review the order. Click on continue to move to the final step i.e. payment.
           </li>
           <li>
             Finally, select the payment method available and click on confirm order.
@@ -53,7 +55,8 @@ const HomePage = (props) => {
             To view the order, hover on Profile menu in the navbar and click on orders.
           </li>
         </ol>
-      </div>
+        {/* </div> */}
+      </Tilt>
     </>
   )
 
