2d5ae369aabed4e0022a52c3eae8f93d9f79122a
dhruv-bhatia246 <71057049+dhruv-bhatia246@users.noreply.github.com>
2024-10-30 02:49:38 +0530

    backend url changed

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,4 +1,4 @@
-const baseURL = "https://e-commerce-rest-backend.herokuapp.com";
+const baseURL = "https://e-commerce-backend-umber-iota.vercel.app/";
 // const baseURL = "http://localhost:2000";
   
 export const api = `${baseURL}/api`;
